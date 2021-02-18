62f7ed70ba8df6779a42be0b0dd89a4478911252
Thelexxirose <cory.balaton@gmail.com>
2021-02-18 09:52:02 +0100

    Updated 2f.py

--- MAT1110/Oblig1/Python/1d.py
+++ MAT1110/Oblig1/Python/1d.py
@@ -8,9 +8,10 @@ b = 3
 # Created a linspace that goes from the bounds defined by the task
 t = np.linspace(-b, b, 1001)
 
-# Defined r(t) as a lambda function taht returns a tuple
+# Defined r(t) as a lambda function that returns a tuple
 r = lambda t: (a*np.arcsinh(t/a), np.sqrt(a**2 + t**2))
 
 # plot r(t)
 plt.plot(r(t)[0], r(t)[1])
-plt.savefig("./images/1d.png")
+# Saves figure using the absolute path of my linux machine
+plt.savefig("/home/cory/Documents/UiO/MAT1110/Oblig1/images/1d.png")
--- MAT1110/Oblig1/Python/2f.py
+++ MAT1110/Oblig1/Python/2f.py
@@ -2,12 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Function that plots a figure with different a,b,c values and output a png file
-def plot(a, b, c, name):
+def plot(a, b, c, name, density):
     plt.clf()
+
+    skip=(slice(None,None,density),slice(None,None,density))
+
+    plt.quiver(x[skip], y[skip], (a*x + b*y)[skip], (c*x - a*y)[skip])
     plt.contour(x, y, c*x**2 - 2*a*x*y - b*y**2, 32)
     plt.axis('equal')
     plt.title(f"a={a}, b={b}, c={c}")
-    plt.savefig(f"./images/{name}.png")
+    plt.savefig(f"/home/cory/Documents/UiO/MAT1110/Oblig1/images/{name}.png")
 
 # Make a linspace to be used for the meshgrid
 I = np.linspace(-30, 30, 1001)
@@ -16,5 +20,5 @@ I = np.linspace(-30, 30, 1001)
 x, y = np.meshgrid(I, I)
 
 # Plot different contour graphs where a,b and c are chosen so that -(a^2 + bc) > 0 and -(a^2 + bc) < 0 
-plot(2,-2, 3, "ellipse")
-plot(2,2, 3, "hyperbolic")
+plot(2,-2, 3, "ellipse", 50)
+plot(2,2, 3, "hyperbolic", 50)
