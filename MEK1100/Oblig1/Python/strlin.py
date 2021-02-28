4f9e4aebaba0bdebb4c256236620b5f0f18bdc39
Cory Balaton <cory.balaton@gmail.com>
2021-03-01 00:13:28 +0100

    Removed Dockerfiles and stopped using abspath

--- MEK1100/Oblig1/Python/strlin.py
+++ MEK1100/Oblig1/Python/strlin.py
@@ -1,10 +1,9 @@
 import numpy as np 
 import matplotlib.pyplot as plt
-import abspath
 from streamfun import streamfun
 
-# Local pypackage that I created to easily access the absolute path of some selected directories.
-path = abspath.get_path("MEK1100") + "/Oblig1/images"
+# Accessing an environment variable that points to the MEK1100 directory
+path = f"{os.getenv('MEK1100')}/Oblig1/images"
 
 # different values of n
 n_vals = [5, 30]
