c8230607a3568636966f0f91bf7ad9b79079f1bd
Cory Balaton <cory.balaton@gmail.com>
2021-03-03 21:09:06 +0100

    Final touches on Oblig 1 MEK1100

--- MEK1100/Oblig1/Python/one_c_revised.py
+++ MEK1100/Oblig1/Python/one_c_revised.py
@@ -2,16 +2,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
+# Accessing an environment variable that points to the MEK1100 directory
 img_path = f"{os.getenv('MEK1100')}/Oblig1/images"
 
+# function that plots x*, y* with t* and different values for theta
 def plot(theta):
     t_star = np.linspace(0,1,101)
 
     x_star = lambda t, theta: t
-    y_star = lambda t, theta: t*(np.sin(theta)/np.cos(theta)) - t**2 * (np.cos(theta)/np.sin(theta))
+    y_star = lambda t, theta: t*(np.tan(theta)) - (t**2*np.tan(theta))
 
     plt.plot(x_star(t_star, theta), y_star(t_star, theta), label=f"Theta={theta:.3f}")
 
+
+# List of different valuesof theta
 theta_list = [np.pi/3, np.pi/4, np.pi/6]
 
 for i in theta_list:
